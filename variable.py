33578caa3cf9905ebf847613749f2bf376c16699
Jitendra Singh <jrathor046@gmail.com>
2026-05-08 08:36:59 +0530

    Initialize variables and print their values
    Add variables for owner name, employee name, age, and profit with print statements.

---
+++ variable.py
@@ -0,0 +1,13 @@
+# store name ,number,float...
+
+Owner_name="jeet"   #Owner_name is variable which stored temporary data/values.
+print(Owner_name)
+
+emp_name="Jitendra"   #emp_name is variable which stored temporary data/values.
+print(emp_name)
+
+age=29
+print("age is :", age,"\n",type(age))
+
+profit=90000.3
+print("profit  is :", profit,"\n",type(profit))
