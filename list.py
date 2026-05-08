3dbcf3813dd591a719d8704a08b4d6e0d04db3c5
Jitendra Singh <jrathor046@gmail.com>
2026-05-08 08:43:25 +0530

    Implement list example with indexing
    Add example usage of list with positive and negative indexing.

---
+++ list.py
@@ -0,0 +1,14 @@
+# list is an order collectiion of elements inclosed within []
+# list is mutable.
+# start from 0  index and end with length-1
+
+person=['jitendra Rathor',22,2000000]
+print(person[0])
+print(person[1])
+print(person[2])
+
+print("\n")
+# Nagative finding:
+# start from -1
+print(person[-2]) 
+print(person[-1]) 
